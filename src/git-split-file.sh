95e8832f02e21595ca001bcc252e63e6b1d6993b
Ben Peachey <potherca@gmail.com>
2018-02-19 20:32:07 +0000

    Removes unused function "indent".

--- src/git-split-file.sh
+++ src/git-split-file.sh
@@ -126,30 +126,6 @@ error() {
 # ==============================================================================
 
 # ==============================================================================
-# sed -l basically makes sed replace and buffer through stdin to stdout
-# so you get updates while the command runs and dont wait for the end
-# e.g. npm install
-# ------------------------------------------------------------------------------
-indent() {
-  # if an arg is given it's a flag indicating we shouldn't indent the first line,
-  # so use :+ to tell SED accordingly if that parameter is set, otherwise null
-  # string for no range selector prefix (it selects from line 2 onwards and then
-  # every 1st line, meaning all lines)
-
-    # @CHECKME: Shouldn't the line below be used?
-    # c="${1:+"2,999"} s/^/       /"
-    c='s/^/       /g'
-    if [[ "$(uname -s)" == "Darwin" ]];then
-        # mac/bsd sed: -l buffers on line boundaries
-        sed -l "$c"
-    else
-        # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
-        sed -u "$c"
-    fi
-}
-# ==============================================================================
-
-# ==============================================================================
 printMessage() {
 # ------------------------------------------------------------------------------
     echo -e "# ${*}" >&1
